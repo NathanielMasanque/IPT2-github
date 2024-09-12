bd721e0cb37cb98ae1ca3082005a2bb388cb8a84
Masanque <samuel@gmail.com>
2024-09-12 14:56:03 +0800

    Division method

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,12 @@ public class Calculator{
       public static int multiply(int a, int b) {
         return a * b;
     }
+     public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
 
     public static void main(String[]args){
 
@@ -15,5 +21,7 @@ public class Calculator{
     System.out.println("Addition: " + add(5, 3));
     System.out.println("Subtraction: " + subtract(5, 3));
     System.out.println("Multiplication: " + multiply(5, 3));
+    System.out.println("Division: " + divide(6, 3));
+
     }
 }
