6d6b2f14cded3246ac850f80e4c5898bfbc01a6b
Masanque <samuel@gmail.com>
2024-09-12 14:54:30 +0800

    Multiplication method

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,15 @@ public class Calculator{
       public static int subtract(int a, int b) {
         return a - b;
       }
+      public static int multiply(int a, int b) {
+        return a * b;
+    }
+
     public static void main(String[]args){
 
     System.out.println("SIMPLE CALCULATOR:");
- System.out.println("Addition: " + add(5, 3));
-System.out.println("Subtraction: " + subtract(5, 3));
+    System.out.println("Addition: " + add(5, 3));
+    System.out.println("Subtraction: " + subtract(5, 3));
+    System.out.println("Multiplication: " + multiply(5, 3));
     }
 }
